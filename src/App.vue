8ec2714b7f078af70382488e3178ef5bd7b7c84a
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-18 20:26:20 +0000

    💄 added base header and styling to app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div>
+  <div class="wrapper">
+    <AppHeader />
   </div>
 </template>
 
+<script lang="ts">
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap");
+
+* {
+  font-family: "Space Mono", monospace;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  height: 100vh;
+}
 
 </style>
