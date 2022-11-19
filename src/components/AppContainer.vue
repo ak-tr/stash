a1550641fb01594732e26210f055a81b817b216b
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-19 03:38:50 +0000

    💄 added upload button

--- src/components/AppContainer.vue
+++ src/components/AppContainer.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="main">
+    <div class="main-header">
+      <ContainerButton text="Upload / ⌘ + ⏎"/>
+    </div>
     <CodeEditor />
   </div>
 </template>
 
 <script lang="ts">
 import CodeEditor from "./CodeEditor.vue";
+import ContainerButton from "./ContainerButton.vue";
 
 export default {
   name: "AppContainer",
   components: {
     CodeEditor,
+    ContainerButton
   }
 }
 </script>
@@ -18,7 +23,22 @@ export default {
 <style>
 .main {
   display: flex;
-  flex-grow: 1;
-  justify-content: center;
+  flex-direction: column;
+  padding: 0 15%;
+  height: 85vh;
+  align-items: center;
+  margin: 20px;
+  box-sizing: border-box;
+}
+
+@media only screen and (max-width: 600px) {
+  .main {
+    padding: 0;
+  }
+}
+
+.main-header {
+  padding: 10px;
+  align-self: flex-end;
 }
 </style>
---
+++ src/components/ContainerButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="button"
+    type="button"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "ContactButton",
+  props: ["text"],
+};
+</script>
+
+<style scoped>
+.button {
+  /* width: 120px; */
+  padding: 0 15px;
+  height: 50px;
+  border: 1px solid white;
+  border-radius: 5px;
+  background-color: #171717;
+  color: white;
+  transition: background-color 0.25s;
+}
+
+.button:hover {
+  background-color: white;
+  color: #171717;
+  cursor: pointer;
+}
+</style>
