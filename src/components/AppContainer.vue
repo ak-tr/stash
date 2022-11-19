6c1d68231e45666743d9cd86ce1d5c3a22a04175
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-19 03:00:40 +0000

    💄 added base container

---
+++ src/components/AppContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <CodeEditor />
+  </div>
+</template>
+
+<script lang="ts">
+import CodeEditor from "./CodeEditor.vue";
+
+export default {
+  name: "AppContainer",
+  components: {
+    CodeEditor,
+  }
+}
+</script>
+
+<style>
+.main {
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+}
+</style>
