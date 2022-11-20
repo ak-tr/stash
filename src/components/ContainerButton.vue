5d8a3b7fc9f4313db360371be75aec1b8f45fa4e
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-20 03:21:11 +0000

    💄 os-based text on button

--- src/components/ContainerButton.vue
+++ src/components/ContainerButton.vue
@@ -1,9 +1,6 @@
 <template>
-  <button
-    class="button"
-    type="button"
-  >
-    {{ text }}
+  <button class="button" type="button">
+    {{ text }} / {{getKeyMap()}}
   </button>
 </template>
 
@@ -11,6 +8,30 @@
 export default {
   name: "ContactButton",
   props: ["text"],
+  methods: {
+    getOS() {
+      if (navigator.userAgent.includes("Win")) return "Win";
+      if (navigator.userAgent.includes("Mac")) return "Mac";
+      if (navigator.userAgent.includes("Linux")) return "Linux";
+      if (
+        navigator.userAgent.includes("Android") ||
+        navigator.userAgent.includes("like Mac")
+      )
+        return "Mobile";
+    },
+    getKeyMap() {
+      switch (this.getOS()) {
+        case "Win":
+          return "Ctrl + ⏎";
+        case "Mac":
+          return "⌘ + ⏎";
+        case "Linux":
+          return "❖ + ⏎";
+        default:
+          return "";
+      }
+    }
+  },
 };
 </script>
 
@@ -18,17 +39,16 @@ export default {
 .button {
   /* width: 120px; */
   padding: 0 15px;
-  height: 50px;
-  border: 1px solid white;
+  height: 40px;
+  border: 1px solid rgba(255, 255, 255, 0.2);
   border-radius: 5px;
   background-color: #171717;
   color: white;
-  transition: background-color 0.25s;
+  transition: border 0.15s;
 }
 
 .button:hover {
-  background-color: white;
-  color: #171717;
+  border: 1px solid white;
   cursor: pointer;
 }
 </style>
