dd46360f697cb8457c69701fea40408def28c8f1
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-21 01:35:58 +0000

    ✨ added route for getting paste

--- worker/src/routes.ts
+++ worker/src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'itty-router'
-import { withContent } from "itty-router-extras";
+import { withContent, withParams } from "itty-router-extras";
 import * as controller from "./controller";
 
 // Create router
 export const router = Router();
 
-router.post("/paste", withContent, controller.createNewPaste);
+// Define main routes
+router
+  .get("/paste/:id", withParams, controller.getPaste)
+  .post("/paste", withContent, controller.createNewPaste);
 
+// Define handler for missing/unknown routes
 router.all("*", controller.missingHandler)
