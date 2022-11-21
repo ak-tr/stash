82974e3f9ca35a84bdafbf05bfc25a508a6e4996
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-21 01:19:48 +0000

    ✨ finished create new paste controller

--- worker/src/controller.ts
+++ worker/src/controller.ts
@@ -1,14 +1,44 @@
+import { Env } from ".";
 import { PasteBody } from "./types";
 
 const headers = {
   "Content-Type": "application/json;charset=UTF-8",
 } as const;
 
-export const createNewPaste = async ({ content }: PasteBody) => {
-  // Get body
-  const { ttl, raw } = content;
+const ttlEpoch = {
+  1: 864000,
+  2: 7200,
+  3: 43200,
+  4: 86400,
+};
 
-  return new Response(JSON.stringify(content, null, 4), { headers });
+export const createNewPaste = async ({ content }: PasteBody, env: Env) => {
+  // Get body
+  const { ttl, raw, once } = content;
+
+  // If missing keys, return bad request
+  if (!(ttl || raw || once)) {
+    return new Response(
+      JSON.stringify({
+        message: "Bad request",
+      }),
+      {
+        headers,
+        status: 400,
+      }
+    );
+  }
+
+  // Generate new ID
+  const uid = Date.now().toString(36);
+  // Calculate expiration
+  const expiration = Date.now() + ttlEpoch[ttl as keyof typeof ttlEpoch];
+
+  // Add to KV
+  await env.STASH_KV.put(uid, JSON.stringify({ raw, once }), { expiration });
+
+
+  return new Response(JSON.stringify({ id: uid }, null, 4), { headers });
 };
 
 export const errorHandler = (error: any) => {
