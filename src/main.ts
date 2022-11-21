f00427d5079ad7631a2b2b726de35965d97931ed
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-21 20:57:18 +0000

    🎨 add axios as global prop

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import axios from "axios";
 
-createApp(App).mount("#app");
+const app = createApp(App)
+app.config.globalProperties.$axios = axios;
+app.mount("#app");
+
