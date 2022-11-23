43ef96fa9b950da93b1d78a2277139aabcbcc121
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-23 19:14:52 +0000

    ✨ added `DELETE` method to cors

--- worker/src/routes.ts
+++ worker/src/routes.ts
@@ -3,7 +3,10 @@ import { withContent, withParams } from "itty-router-extras";
 import { createCors } from "itty-cors";
 import * as controller from "./controller";
 
-const { preflight, corsify } = createCors({ origins: ["*"] });
+const { preflight, corsify } = createCors({
+  methods: ["GET", "POST", "DELETE"],
+  origins: ["*"] 
+});
 
 // Create router
 const router = Router();
