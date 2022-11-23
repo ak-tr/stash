f0fe62e6a97e28591cf48cce3fdc701f30718a59
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-23 18:29:17 +0000

    🎨 moved upload key string logic outside of component

--- app/src/components/ContainerButton.vue
+++ app/src/components/ContainerButton.vue
@@ -1,61 +1,53 @@
 <template>
   <button class="button" type="button" ref="button">
-    {{ text }} / {{getKeyMap()}}
+    {{ buttonText }}
   </button>
 </template>
 
 <script lang="ts">
 export default {
   name: "ContactButton",
-  props: ["text"],
+  data() {
+    return {
+      buttonText: this.text,
+    }
+  },
+  props: ["text", "afterText"],
   mounted() {
     const button = this.$refs.button as HTMLButtonElement;
 
     button.addEventListener("click", () => {
-      this.$emit("uploadClick");
-    })
-  },
-  methods: {
-    getOS() {
-      if (navigator.userAgent.includes("Win")) return "Win";
-      if (navigator.userAgent.includes("Mac")) return "Mac";
-      if (navigator.userAgent.includes("Linux")) return "Linux";
-      if (
-        navigator.userAgent.includes("Android") ||
-        navigator.userAgent.includes("like Mac")
-      )
-        return "Mobile";
-    },
-    getKeyMap() {
-      switch (this.getOS()) {
-        case "Win":
-          return "Ctrl + ⏎";
-        case "Mac":
-          return "⌘ + ⏎";
-        case "Linux":
-          return "❖ + ⏎";
-        default:
-          return "";
+      if (this.afterText) {
+        this.buttonText = this.afterText;
+        button.disabled = true;
       }
-    }
+
+      this.$emit("btnClick");
+    })
+
+
   },
 };
 </script>
 
 <style scoped>
 .button {
-  /* width: 120px; */
+  min-width: 120px;
   padding: 0 15px;
   height: 40px;
   border: 1px solid rgba(255, 255, 255, 0.2);
   border-radius: 5px;
   background-color: #171717;
   color: white;
-  transition: border 0.15s;
+  transition: border 0.15s, color 0.35s, border 0.35s;
 }
 
-.button:hover {
+.button:hover:enabled {
   border: 1px solid white;
   cursor: pointer;
+}
+
+.button:disabled {
+  color:  rgba(255, 255, 255, 0.2);
 }
 </style>
