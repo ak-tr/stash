68c02cdcda622ddc1864279565b7812c15045768
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-23 15:34:20 +0000

    ✨ added component for displaying message to user

---
+++ app/src/components/ContainerMessage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="message">
+    <h1>{{header}}</h1>
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "ContainerMessage",
+  props: [
+    "header",
+    "message"
+  ]
+}
+</script>
+
+<style scoped>
+.message {
+  max-width: 550px;
+  padding-top: 20px;
+}
+
+h1 {
+  font-size: 300%;
+  margin: 0;
+  line-height: 50px;
+}
+</style>
