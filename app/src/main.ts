3109abf46fcc9e32c4e94b3af16f7fdfbf1757b0
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-27 18:40:13 +0000

    🔥 removed axios from global prop

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,14 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import axios from "axios";
 
 const app = createApp(App)
-app.config.globalProperties.$axios = axios;
 app.mount("#app");
-
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $axios: typeof axios
-  }
-}
