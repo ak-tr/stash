72d94b2c4ddef53cf4425d3f5f1682b11c5378a9
ak-tr <62529128+ak-tr@users.noreply.github.com>
2022-11-27 02:34:37 +0000

    ✨ helper file for syntax highlighting

---
+++ app/src/helpers/highlighters.ts
@@ -0,0 +1,17 @@
+import { typescript, javascript } from "@codemirror/legacy-modes/mode/javascript"
+import { go } from "@codemirror/legacy-modes/mode/go"
+import { python } from "@codemirror/legacy-modes/mode/python"
+import { java, kotlin, c, cpp, csharp, scala } from "@codemirror/legacy-modes/mode/clike"
+
+export const modes = {
+  typescript,
+  javascript,
+  python,
+  go,
+  java,
+  kotlin,
+  c,
+  cpp,
+  csharp,
+  scala
+}
